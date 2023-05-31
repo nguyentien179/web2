712b709afc8d9e4af2de4d3b79f6390c8df2db75
nguyentien179 <88731661+nguyentien179@users.noreply.github.com>
2023-05-31 13:45:17 +0700

    m

--- lab08/app.js
+++ lab08/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var studentsRouter = require('./routes/students');
 
 var app = express();
 var mongoose = require('mongoose');
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/students', studentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
